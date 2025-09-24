a66ab5b8cd5296b8e6e2348267ebaa6c59d9b8d2
minercoins <mnrcoins@gmail.com>
2025-09-24 11:15:47 +0700

    fix: ganti <a> jadi <Link>

--- miner-bep-chart/src/components/Navbar.js
+++ miner-bep-chart/src/components/Navbar.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,16 +21,24 @@ export default function Navbar() {
 
         {/* Menu desktop */}
         <ul className="hidden md:flex gap-4">
-          <li><a href="/" className="hover:underline">Home</a></li>
-          <li><a href="/about" className="hover:underline">About</a></li>
+          <li><Link href="/" className="hover:underline">Home</Link></li>
+          <li><Link href="/about" className="hover:underline">About</Link></li>
         </ul>
       </div>
 
       {/* Menu mobile */}
       {isOpen && (
         <ul className="md:hidden mt-2 flex flex-col gap-2">
-          <li><a href="/" className="block p-2 hover:bg-gray-700 rounded">Home</a></li>
-          <li><a href="/about" className="block p-2 hover:bg-gray-700 rounded">About</a></li>
+          <li>
+            <Link href="/" className="block p-2 hover:bg-gray-700 rounded">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="block p-2 hover:bg-gray-700 rounded">
+              About
+            </Link>
+          </li>
         </ul>
       )}
     </nav>
